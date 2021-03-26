ba196cd593f052a18c7c6f9e295603542acec8cb
Mike Pugach <kegach@yandex.ru>
2021-03-26 22:44:59 +0300

    Diplom console error text

--- middlewares/errorOnServer.js
+++ middlewares/errorOnServer.js
@@ -2,7 +2,7 @@ const errorOnServer = (err, req, res, next) => {
   const { statusCode = 500, message } = err;
   res.status(statusCode).send({
     message: statusCode === 500
-      ? req.body
+      ? err.message
       : message,
   });
   next();
