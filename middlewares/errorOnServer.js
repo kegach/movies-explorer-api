f992a2fc92f5338f80d32727fc98f8311ed063a8
Mike Pugach <kegach@yandex.ru>
2021-03-27 00:14:48 +0300

    Diplom final

--- middlewares/errorOnServer.js
+++ middlewares/errorOnServer.js
@@ -2,7 +2,7 @@ const errorOnServer = (err, req, res, next) => {
   const { statusCode = 500, message } = err;
   res.status(statusCode).send({
     message: statusCode === 500
-      ? err.message
+      ? 'На сервере произошла ошибка'
       : message,
   });
   next();
